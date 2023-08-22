9dca884c825446c4cac6925e06916e8cd1438f0a
Thanatix <thomaslambert@kinetix.tech>
2023-08-22 18:42:22 +0200

    1.1.0

--- Editor/PackageManagement/Modules/KinetixModules.cs
+++ Editor/PackageManagement/Modules/KinetixModules.cs
@@ -13,20 +13,12 @@ namespace Kinetix.PackageManager.Editor
             GitURL      = c_CORE_URL + "kinetix-unity-sdk-packagemanager.git"
         };
 
-        public static readonly KinetixModuleInfo CoreWeb3Module = new KinetixModuleInfo()
-        {
-            DisplayName = "Kinetix Core Web3",
-            Name        = "com.kinetix.coreweb3",
-            GitURL      = c_CORE_URL + "kinetix-unity-sdk-coreweb3.git",
-            Version     = "1.0.1"
-        };
-        
         public static readonly KinetixModuleInfo CoreWeb2Module = new KinetixModuleInfo()
         {
             DisplayName = "Kinetix Core Web2",
             Name        = "com.kinetix.coreweb2",
             GitURL      = c_CORE_URL + "kinetix-unity-sdk-coreweb2.git",
-            Version     = "1.0.1"
+            Version     = "1.1.0"
         };
         
         public static readonly KinetixModuleInfo UICommonModule = new KinetixModuleInfo()
@@ -34,7 +26,7 @@ namespace Kinetix.PackageManager.Editor
             DisplayName = "Kinetix UI Common",
             Name        = "com.kinetix.uicommon",
             GitURL      = c_CORE_URL + "kinetix-unity-sdk-uicommon.git",
-            Version     = "1.0.1"
+            Version     = "1.1.0"
         };
         
         public static readonly KinetixModuleInfo UIEmoteWheelModule = new KinetixModuleInfo()
@@ -42,7 +34,7 @@ namespace Kinetix.PackageManager.Editor
             DisplayName = "Kinetix UI Emote Wheel",
             Name        = "com.kinetix.uiemotewheel",
             GitURL      = c_CORE_URL + "kinetix-unity-sdk-uiemotewheel.git",
-            Version     = "1.0.1"
+            Version     = "1.1.0"
         };
 
         public static readonly KinetixBundleInfo CoreBundleWeb3 = new KinetixBundleInfo()
@@ -50,9 +42,28 @@ namespace Kinetix.PackageManager.Editor
             Name = "[WEB3] Emote Wheel Bundle",
             Modules = new[]
             {
-                CoreWeb3Module,
-                UICommonModule,
-                UIEmoteWheelModule
+                
+                new KinetixModuleInfo()
+                {
+                    DisplayName = "Kinetix Core Web3",
+                    Name        = "com.kinetix.coreweb3",
+                    GitURL      = c_CORE_URL + "kinetix-unity-sdk-coreweb3.git",
+                    Version     = "0.7.2"
+                },
+                new KinetixModuleInfo()
+                {
+                    DisplayName = "Kinetix UI Common",
+                    Name        = "com.kinetix.uicommon",
+                    GitURL      = c_CORE_URL + "kinetix-unity-sdk-uicommon.git",
+                    Version     = "0.7.1"
+                },
+                new KinetixModuleInfo()
+                {
+                    DisplayName = "Kinetix UI Emote Wheel",
+                    Name        = "com.kinetix.uiemotewheel",
+                    GitURL      = c_CORE_URL + "kinetix-unity-sdk-uiemotewheel.git",
+                    Version     = "0.7.2"
+                }
             }
         };
         
