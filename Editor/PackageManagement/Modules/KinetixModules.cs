a31df9183f207c611c1fe5206def6fdc9e5465e7
Thanatix <thomaslambert@kinetix.tech>
2023-12-04 16:21:05 +0100

    1.3.0

--- Editor/PackageManagement/Modules/KinetixModules.cs
+++ Editor/PackageManagement/Modules/KinetixModules.cs
@@ -18,7 +18,7 @@ namespace Kinetix.PackageManager.Editor
             DisplayName = "Kinetix Core Web2",
             Name        = "com.kinetix.coreweb2",
             GitURL      = c_CORE_URL + "kinetix-unity-sdk-coreweb2.git",
-            Version     = "1.2.0"
+            Version     = "1.3.0"
         };
         
         public static readonly KinetixModuleInfo UICommonModule = new KinetixModuleInfo()
@@ -26,7 +26,7 @@ namespace Kinetix.PackageManager.Editor
             DisplayName = "Kinetix UI Common",
             Name        = "com.kinetix.uicommon",
             GitURL      = c_CORE_URL + "kinetix-unity-sdk-uicommon.git",
-            Version     = "1.2.0"
+            Version     = "1.3.0"
         };
         
         public static readonly KinetixModuleInfo UIEmoteWheelModule = new KinetixModuleInfo()
@@ -34,7 +34,7 @@ namespace Kinetix.PackageManager.Editor
             DisplayName = "Kinetix UI Emote Wheel",
             Name        = "com.kinetix.uiemotewheel",
             GitURL      = c_CORE_URL + "kinetix-unity-sdk-uiemotewheel.git",
-            Version     = "1.2.0"
+            Version     = "1.3.0"
         };
 
         public static readonly KinetixBundleInfo CoreBundleWeb2 = new KinetixBundleInfo()
